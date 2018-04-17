e4a8fe288aca9dd1cd2c301d19f179805b11e308
Meghna Srivastava <meghna.s1906@gmail.com>
2018-04-17 18:10:29 +0200

    initialised state

--- react-strap/src/App.js
+++ react-strap/src/App.js
@@ -7,6 +7,13 @@ import Jumbotron from './components/Jumbotron';
 import Page from './components/Page';
 
 class App extends Component {
+  state = {
+    currentPage: "about",
+    brand: "ReactStrap",
+    heading: "Hello world",
+    text: "This is a Bootstrap theme that uses React.js components"
+
+  }
   // getDefaultProps() {
   //   return {
   //     brand: "DefaultBrand"
@@ -15,9 +22,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar currentPage="home" brand="ReactStrap" />
-        <Jumbotron heading="Hello world" text="This is a Bootstrap theme that uses React.js components" />
-        <Page currentPage="home" />
+        <NavBar currentPage={this.state.currentPage} brand={this.state.brand} />
+        <Jumbotron heading={this.state.heading} text={this.state.text} />
+        <Page currentPage={this.state.currentPage} />
       </div>
     );
   }
